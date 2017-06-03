3cf656216d885710ee942f2f2fec70492639809c
kala888 <kala.niubility@gmail.com>
2017-06-03 18:03:10 +0800

    add beep

--- src/main/java/com/rfidsample/RFIDReader.java
+++ src/main/java/com/rfidsample/RFIDReader.java
@@ -48,6 +48,8 @@ public class RFIDReader {
                     continue;
                 }
                 dev.setDeviceCode(deviveCode);
+                dev.beep(3, 10);
+                break;
         }
     }
 
